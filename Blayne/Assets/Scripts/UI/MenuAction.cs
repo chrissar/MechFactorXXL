8e403d236c9535e2f4a5bc043ead047278d942af
Arseni Natapov <arseni.natapov@gmail.com>
2016-04-09 09:02:39 -0400

    Added basic menu functionality
    Added SpawnMenu class which is responsible for processing mouse input during Top-Down perspective.
    Added MenuAction class which is a base wrapper class for various AI actions.

---
+++ Blayne/Assets/Scripts/UI/MenuAction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MenuActions
+{
+    public class MenuAction : MonoBehaviour
+    {
+        public SpawnMenu.ActionTarget selectedType;
+        public SpawnMenu.ActionTarget targetType;
+        private Button button;
+        protected Vector3 selectedLocation, targetLocation;
+        protected GameObject selectedObject, targetObject;
+        public void Awake()
+        {
+            button = GetComponent<Button>();
+            if (!button) throw new UnityException("A Menu Action is attached to an object that is not a button!");
+            button.onClick.AddListener(delegate { OnClick(); });
+        }
+        private void OnClick()
+        {
+            SpawnMenu.instance.CopySelectionData(out selectedLocation, out targetLocation, out selectedObject, out targetObject);
+            Execute();
+        }
+        protected virtual void Execute()
+        {
+
+        }
+    }
+}
---
+++ Blayne/Assets/Scripts/UI/SpawnMenu.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MenuActions;
+public class SpawnMenu : MonoBehaviour
+{
+    public enum ActionTarget
+    {
+        Friend,
+        FriendGroup,
+        Enemy,
+        EnemyGroup,
+        Nothing
+    }
+    public GameObject menuPrefab;
+    public Camera viewCamera;
+    public static SpawnMenu instance
+    {
+        get;
+        private set;
+    }
+    private Canvas _canvas;
+    private List<GameObject> lastSpawnedMenu;
+    private MenuAction[] allActions;
+
+    private Vector3 selectedLocation;
+    private Vector3 targetLocation;
+    private GameObject selectedObject;
+    private GameObject targetObject;
+    private ActionTarget selectedType = ActionTarget.Nothing;
+    private ActionTarget targetType = ActionTarget.Nothing;
+	// Use this for initialization
+	void Awake ()
+    {
+        instance = this;
+        allActions = menuPrefab.GetComponentsInChildren<MenuAction>();
+        lastSpawnedMenu = new List<GameObject>();
+        _canvas = GetComponent<Canvas>();
+        if (!viewCamera) throw new UnityException("No View Camera attached to the SpawnMenu Script.");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            SelectObject(out selectedObject, out selectedType, out selectedLocation);
+        }
+	    if(Input.GetMouseButtonDown(1))
+        {
+            SelectObject(out targetObject, out targetType, out targetLocation);
+            CreateMenu();
+        }
+	}
+    private void SelectObject(out GameObject obj, out ActionTarget type, out Vector3 location)
+    {
+        RaycastHit hit = CastRayOnMouse();
+        obj = hit.collider ? hit.collider.gameObject : null;
+        location = hit.point;
+        type = ActionTarget.Nothing;
+    }
+    private List<MenuAction> GetAllActions(ActionTarget selected, ActionTarget target)
+    {
+        List<MenuAction> result = new List<MenuAction>();
+        foreach(MenuAction action in allActions)
+        {
+            if(action.selectedType == selected && action.targetType == target)
+            {
+                result.Add(action);
+            }
+        }
+        return result;
+    }
+    private void CreateMenu()
+    {
+        if(lastSpawnedMenu.Count > 0)
+        {
+            foreach(GameObject menuItem in lastSpawnedMenu)
+            {
+                Destroy(menuItem);
+            }
+            lastSpawnedMenu.Clear();
+        }
+        List<MenuAction> actions = GetAllActions(selectedType, targetType);
+        Vector3 position = Input.mousePosition - GetRectSize(_canvas.pixelRect) * 0.5f;
+        float verticalOffset = 0.0f;
+        foreach (MenuAction action in actions)
+        {
+            GameObject item = Instantiate(action.gameObject);
+            lastSpawnedMenu.Add(item);
+            item.transform.SetParent(_canvas.transform);
+            RectTransform itemRect = item.GetComponent<RectTransform>();
+            Vector3 rectSize = GetRectSize(itemRect.rect);
+            Vector3 invertedYRectSize = new Vector3(rectSize.x, -rectSize.y, rectSize.z) * 0.5f - Vector3.up * verticalOffset;
+            verticalOffset += rectSize.y;
+            itemRect.localPosition = position + invertedYRectSize;
+        }
+    }
+    private Vector3 GetRectSize(Rect rect)
+    {
+        Vector2 size = rect.size;
+        return new Vector3(size.x, size.y, 0);
+    }
+    private RaycastHit CastRayOnMouse()
+    {
+        RaycastHit result;
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out result);
+        return result;
+    }
+    public void CopySelectionData(
+        out Vector3 selectedLocation, 
+        out Vector3 targetLocation, 
+        out GameObject selectedObject, 
+        out GameObject targetObject)
+    {
+        selectedLocation = this.selectedLocation;
+        targetLocation = this.targetLocation;
+        selectedObject = this.selectedObject;
+        targetObject = this.targetObject;
+    }
+}
