ef4bb1cfb888209e79ae0bac2832c90cc227270c
Arseni Natapov <arseni.natapov@gmail.com>
2016-04-10 11:53:00 -0400

    Converted Attack and Disengage Commands to Menu Actions
    Added a GetAllMembers() function to FireTeam class that returns all members, including the leader.
    Changed all previous MenuActions to reference Command code.
    Added TeamAttackEnemyCommand and TeamDisengageCommand that reuses Attack and Disengage commands and relays them to the team.
    Added TeamAttackEnemyMenuAction and TeamDisengageMenuAction that wraps the beforementioned classes and uses them for menu.
    Added the new Menu Actions to the menu prefab.

---
+++ Blayne/Assets/Scripts/AI/Commands/TeamAttackEnemyCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class TeamAttackEnemyCommand : AttackEnemyCommand
+{
+    public TeamAttackEnemyCommand(FireTeam enemyTeam)
+        : base(enemyTeam)
+    {
+
+    }
+    public override void execute(Ally ally)
+    {
+        if(ally && ally is FireTeam)
+        {
+            FireTeam team = ally as FireTeam;
+            var allies = team.GetAllMembers();
+            foreach(FireTeamAlly nextAlly in  allies)
+            {
+                base.execute(nextAlly);
+            }
+        }
+    }
+}
