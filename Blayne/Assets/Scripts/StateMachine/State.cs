8bd3ce3b457ca6d0af8c70ee9c46236206e6936c
geoffrey-liu <geoffrey.p.liu@gmail.com>
2016-04-10 14:55:18 -0400

    Added code for simple state machine

---
+++ Blayne/Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class State
+{
+    private List<StateTransition> _transitions;
+    public string Name
+    {
+        get; set;
+    }
+    private Action stateAction;
+    public State(string name, Action action)
+    {
+        Name = name;
+        stateAction = action;
+        _transitions = new List<StateTransition>();
+    }
+    public void AddTransition(StateTransition transition)
+    {
+        _transitions.Add(transition);
+    }
+    public State Transition()
+    {
+        State nextState = this;
+        foreach (StateTransition transition in _transitions)
+        {
+            if (transition.IsReady())
+            {
+                transition.Activate();
+                nextState = transition.NextState;
+                break;
+            }
+        }
+        return nextState;
+    }
+    public void Act()
+    {
+        if (stateAction != null)
+        {
+            stateAction.Invoke();
+        }
+    }
+}
