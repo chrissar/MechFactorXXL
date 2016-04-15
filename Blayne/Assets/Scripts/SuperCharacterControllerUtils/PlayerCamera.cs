82606182aaa81c8dd6d90d17426b34c473a734bf
Ratslayer <arseni.natapov@gmail.com>
2016-04-14 23:36:36 -0400

    Added panning and zooming to the Top-Down View camera
    Added a debug object slot for SpawnMenu, due to all problems
    with the camera raytracing
    Disabled the camera created by PlayerMachine. Removing it wasn't
    an option but everything seems to be working fine without it.

--- Blayne/Assets/Scripts/SuperCharacterControllerUtils/PlayerCamera.cs
+++ Blayne/Assets/Scripts/SuperCharacterControllerUtils/PlayerCamera.cs
@@ -10,15 +10,20 @@ public class PlayerCamera : MonoBehaviour
     public float rotationSpeed = 1.0f;
     public float topDownHeight = 10.0f;
     public float crouchHeight = 1.0f;
-
+    public float panningSpeed = 2.0f;
+    public float zoomingSpeed = 1.0f;
+    public float minZoom = 0.3f;
+    public float maxZoom = 3;
     public GameObject PlayerTarget;
 
     private PlayerInputController input;
     private PlayerMachine machine;
     private float yRotation;
-
+    private float mZoom = 1.0f;
+    private Vector3 mPannedOffset;
     private SuperCharacterController controller;
-
+    private Vector3 lastMousePos;
+    private Vector3 currentMousePos;
 	// Use this for initialization
 	void Start ()
     {
@@ -32,8 +37,12 @@ public class PlayerCamera : MonoBehaviour
     {
         Vector3 pos;
         Quaternion rot;
+        lastMousePos = currentMousePos;
+        currentMousePos = Input.mousePosition;   
         if (!GameController.Instance.topDownView)
         {
+            mZoom = 1.0f;
+            mPannedOffset = Vector3.zero;
             Transform target = PlayerTarget.transform;
 
             yRotation += input.Current.MouseInput.y;
@@ -79,7 +88,15 @@ public class PlayerCamera : MonoBehaviour
         }
         else
         {
-            transform.position = PlayerTarget.transform.position + Vector3.up * topDownHeight;
+            mZoom -= Input.mouseScrollDelta.y * 0.1f;
+            mZoom = Mathf.Clamp((mZoom) * zoomingSpeed, minZoom, maxZoom);
+            if (Input.GetMouseButton(2))
+            {
+                Debug.Log("PANNING!!!!!");
+                Vector3 deltaMouse = currentMousePos - lastMousePos;
+                mPannedOffset += new Vector3(deltaMouse.x, 0, deltaMouse.y) * panningSpeed * mZoom;
+            }
+            transform.position = PlayerTarget.transform.position + Vector3.up * topDownHeight * mZoom - mPannedOffset;
             transform.rotation = Quaternion.LookRotation(-Vector3.up, Vector3.forward);
         }
         //gameObject.transform.position = Vector3.Lerp(pos, gameObject.transform.position, Time.deltaTime * movementSpeed);
