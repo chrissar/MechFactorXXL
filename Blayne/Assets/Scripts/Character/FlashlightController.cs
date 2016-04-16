7089302a5c4f81564d08b42b480e276978c3b4cb
chrissar <christopherricha90@gmail.com>
2016-04-15 20:07:21 -0400

    Flashlight and Collision
    - Provides collision detections to the levels.
    - Provides a Flashlight for the player character. Hit the F key to make
    it appear. Works best in dark areas ;)

---
+++ Blayne/Assets/Scripts/Character/FlashlightController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightController : MonoBehaviour {
+
+	public bool flashlight = false;
+	public GameObject spotlight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			flashlight = !flashlight;
+		}
+		if (flashlight) {
+			spotlight.SetActive (true);
+		} else {
+			spotlight.SetActive (false);
+		}
+	}
+}
